6be4f4bb15a78e9803bba8468dbc74098f063bdc
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.gov.pl>
2019-01-04 11:04:22 +0100

    Add identification of species and voxels

--- analiza.py
+++ analiza.py
@@ -1,34 +1,18 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[3]:
-
-
 import h5py
-
-
-# In[12]:
-
-
+import numpy as np
+from lxml import etree
 my_file = h5py.File('model_start.h5', 'r')
-
-
-# In[13]:
-
-
-for g in my_file['model']['grid']:
-    print(g)
-                 
-
-
-# In[14]:
-
-
-list(my_file['model']['grid'])
-
-
-# In[ ]:
-
-
-
-
+grid_list = np.array(my_file['model']['grid'])
+#times, assuming that there is only one trial, trial0
+times = np.array(my_file['trial0']['output']['__main__']['times'])
+#time, voxel, specie
+data = np.array(my_file['trial0']['output']['__main__']['population'])
+subvolumes = list(my_file['model']['output']['__main__']['elements'])
+species = list(my_file['model']['output']['__main__']['species'])
+#1. calculate volume
+regions = list(set([grid[15] for grid in grid_list]))
+print(list(my_file['model'].keys()))
+xml_input = my_file['model']['serialized_config'][0]
+print(xml_input)
