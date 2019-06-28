703d4abc52a5bd5e3699e1bb9cc8a6ebecbbe801
Asia Szmek <asia@in.waw.pl>
2019-06-28 17:21:45 +0200

    Back Ca injections and buffer

--- stimulations/redo_HFS.py
+++ stimulations/redo_HFS.py
@@ -135,7 +135,7 @@ if __name__ == "__main__":
     train = parse_root(root)
     trains_4x3s = make_trains(train, 4, isi=3000)
     new_root = make_xml(trains_4x3s)
-    new_new_root = change_1_HFS_train(new_root, "CaCbuf", "rate",
+    new_new_root = change_1_HFS_train(new_root, "Ca", "rate",
                                       region="sa1[0].pointA",
                                       multiplier=2.3,
                                       addition=0)
@@ -146,7 +146,7 @@ if __name__ == "__main__":
     xml_write_to_file("4xHFS_3s.xml", new_new_root)
     trains_4x80s = make_trains(train, 4, isi=80000)
     new_root = make_xml(trains_4x80s)
-    new_new_root = change_1_HFS_train(new_root, "CaCbuf", "rate",
+    new_new_root = change_1_HFS_train(new_root, "Ca", "rate",
                                       region="sa1[0].pointA",
                                       multiplier=2.3,
                                       addition=0)
@@ -156,7 +156,7 @@ if __name__ == "__main__":
                                       addition=0)
     
     xml_write_to_file("4xHFS_80s.xml", new_new_root)
-    new_root = change_1_HFS_train(root, "CaCbuf", "rate",
+    new_root = change_1_HFS_train(root, "Ca", "rate",
                                   region="sa1[0].pointA",
                                   multiplier=2.3,
                                   addition=0)
