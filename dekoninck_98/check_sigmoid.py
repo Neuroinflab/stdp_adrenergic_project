c86660a3977e057dc7d6ccdf7aeb7a3f9a6978b3
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.gov.pl>
2019-07-01 14:10:36 +0200

    Plot sigmoid

---
+++ dekoninck_98/check_sigmoid.py
@@ -0,0 +1,26 @@
+from __future__ import print_function, division
+
+import matplotlib.pyplot as plt
+import numpy as np
+flist = [
+    "model_start_100_trial0_dend.txt",
+    "model_start_300_trial0_dend.txt",
+    "model_start_1000_trial0_dend.txt",
+    "model_start_5000_trial0_dend.txt",
+    ]
+
+if __name__ == "__main__":
+
+    out = []
+    for fname in flist:
+        f = open(fname)
+        header = f.readline().split()
+        data = np.loadtxt(f)
+        CKp = max(data[:, header.index('CKp')])
+        out.append(CKp)
+    out = np.array(out)
+    out = out/out.max()
+    plt.figure()
+    plt.plot([100, 300, 1000, 5000], out)
+    plt.xlabel('CaMCa4 (nM)')
+    plt.show()
