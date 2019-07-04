ed0aaf0618b63429daa8af6336ef57af635c6f6d
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.gov.pl>
2019-07-04 11:22:57 +0200

    Lower Ca for 2 last trains of 4 trains massed

--- stimulations/redo_HFS.py
+++ stimulations/redo_HFS.py
@@ -81,9 +81,9 @@ def change_1_HFS_train(root, specie, what, region=None, multiplier=1, addition=0
                 for grandson in son:
                     if grandson.tag == "onset":
                         onset = float(grandson.text)
-                        if onset > previous_onset +  3000:
+                        if onset > previous_onset +  5000:
                             counter = 0
-                        previous_onset = onset
+                            previous_onset = onset
                     if grandson.tag == what:
                         if '.' in grandson.text:
                             new_value = float(grandson.text)
@@ -93,6 +93,7 @@ def change_1_HFS_train(root, specie, what, region=None, multiplier=1, addition=0
                             new_value = multiplier*new_value + addition
                         else:
                             new_value = 0
+                        print(counter, new_value)
                         grandson.text = str(np.round(new_value, decimals=2))
     return root
         
@@ -144,24 +145,24 @@ if __name__ == "__main__":
                                       multiplier=1,
                                       addition=0)
     xml_write_to_file("4xHFS_3s.xml", new_new_root)
-    trains_4x80s = make_trains(train, 4, isi=80000)
-    new_root = make_xml(trains_4x80s)
-    new_new_root = change_1_HFS_train(new_root, "Ca", "rate",
-                                      region="sa1[0].pointA",
-                                      multiplier=2.3,
-                                      addition=0)
-    new_new_root = change_1_HFS_train(new_new_root, "CaB", "rate",
-                                      region="sa1[0].pointA",
-                                      multiplier=1,
-                                      addition=0)
+    # trains_4x80s = make_trains(train, 4, isi=80000)
+    # new_root = make_xml(trains_4x80s)
+    # new_new_root = change_1_HFS_train(new_root, "Ca", "rate",
+    #                                   region="sa1[0].pointA",
+    #                                   multiplier=2.3,
+    #                                   addition=0)
+    # new_new_root = change_1_HFS_train(new_new_root, "CaB", "rate",
+    #                                   region="sa1[0].pointA",
+    #                                   multiplier=1,
+    #                                   addition=0)
     
-    xml_write_to_file("4xHFS_80s.xml", new_new_root)
-    new_root = change_1_HFS_train(root, "Ca", "rate",
-                                  region="sa1[0].pointA",
-                                  multiplier=2.3,
-                                  addition=0)
-    new_root = change_1_HFS_train(new_root, "CaB", "rate",
-                                  region="sa1[0].pointA",
-                                  multiplier=1,
-                                  addition=0)
-    xml_write_to_file("HFS.xml", new_root)
+    # xml_write_to_file("4xHFS_80s.xml", new_new_root)
+    # new_root = change_1_HFS_train(root, "Ca", "rate",
+    #                               region="sa1[0].pointA",
+    #                               multiplier=2.3,
+    #                               addition=0)
+    # new_root = change_1_HFS_train(new_root, "CaB", "rate",
+    #                               region="sa1[0].pointA",
+    #                               multiplier=1,
+    #                               addition=0)
+    # xml_write_to_file("HFS.xml", new_root)
