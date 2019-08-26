32283c3f73043dc724a39a481383c83a7a2203a2
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.gov.pl>
2019-08-26 14:25:21 +0200

    Deal with unfinished simulations

--- cAMP_validation/make_pnmdar_figures.py
+++ cAMP_validation/make_pnmdar_figures.py
@@ -2,7 +2,7 @@ from __future__ import print_function, division, unicode_literals
 import numpy as np
 import matplotlib.pyplot as plt
 import glob
-fnames_DA_pnmdar = glob.glob("model_DA_bath_20_um_trial?_PSD.txt")
+fnames_DA_pnmdar = glob.glob("model_DA_bath_20_uM_trial?_PSD.txt")
 fnames_ISO_pnmdar = glob.glob("model_ISO_bath_trial?_PSD.txt")
 fnames_control_pnmdar = glob.glob("../model_start_trial?_PSD.txt")
 
@@ -10,7 +10,7 @@ fnames_DA_epacs = glob.glob("model_DA_bath_trial?_total.txt")
 fnames_ISO_epacs = glob.glob("model_ISO_bath_trial?_total.txt")
 fnames_control_epacs = glob.glob("../model_start_trial?_total.txt")
 
-labels = ["DA bath", "ISO bath", "control"]
+labels = [u"Dopamine 20 μM", u"Isoproterenol 1 μM", "control"]
 
 list1 = [fnames_DA_epacs, fnames_ISO_epacs]
 
@@ -22,8 +22,12 @@ titles = ["cAMP activity",
 fnames = ["cAMP_activity.svg",
           "NMDAR_phosphorylation.svg"]
 
-colors = ["b", "g", "k"]
+colors = ["dodgerblue", "forestgreen", "k"]
 specie = ["Epac1cAMP", "pNMDAR"]
+
+plt.rc('xtick',labelsize=14)
+plt.rc('ytick',labelsize=14)
+#plt.rc('legend',fontsize=14)
 
 def read_in_data(f_list, specie):
     out = []
@@ -61,9 +65,11 @@ def make_time(arrs):
     if np.all(lens == lens[0]):
         return arrs[0]
     argmin = np.argmin(lens)
-    if isinstance(argmin, int):
+
+    try: 
         return arrs[argmin]
-    return arrs[argmin[0]]
+    except:
+        return arrs[argmin[0]]
 
 
 def make_data(f_list, specie, control=False):
@@ -77,21 +83,17 @@ if __name__ == '__main__':
 
     for i, par in enumerate(lists):
         title = titles[i]
-        fig, ax = plt.subplots(1, 1, figsize=(10,10))
+        fig, ax = plt.subplots(1, 1)
         for j, flist in enumerate(par):
             time, mean, error = make_data(flist, specie[i], control=not i)
-            
             ax.plot(time/1000, mean,
                     color=colors[j], label=labels[j])
             if i:
-                ax.plot(time/1000, (mean-800)/800,
-                        color=colors[j], label=labels[j])
-                
-        
+                ax.plot(time/1000, 800*np.ones_like(time), "k")
         ax.legend(loc=3)
-        ax.set_xlabel("time (s)")
-        ax.set_title(title[i])
-        #ax.set_ylabel(ylabel[i])
+        ax.set_xlabel("time (s)", fontsize=14)
+        ax.set_ylabel("% change", fontsize=14)
+        ax.set_title(titles[i], fontsize=20)
         fig.savefig(fnames[i], bbox_inches='tight',
                     transparent=True, pad_inches=0.1)
         
